681d1282ce4e42ffc9c00a339363ca83e6da01d5
jaemin.han <jaemin.han@nyu.edu>
2018-07-08 01:23:47 -0400

    search input - filter organization complete

--- src/DonationLists.js
+++ src/DonationLists.js
@@ -1,37 +1,72 @@
-import React from 'react';
+import React, { Component }from 'react';
 import PropTypes from 'prop-types';
 
-class DonationLists extends React.Component {
+class DonationLists extends Component {
+    //PropTypes to a class Component
+    static propTypes = {
+        locations: PropTypes.array.isRequired,
+        onDeleteLocation: PropTypes.func.isRequired
+    }
+    
+    state = {
+        search: ''
+    }
+
+    updateSearch = (search) => {
+        this.setState(() => ({
+            search: search.trim()
+        }))
+    }
     render() {
+        const { search } = this.state
+        const { locations, onDeleteLocation } = this.props
+
+        const showingAllOrganization = search === ''
+            ? locations
+            : locations.filter((update) => (
+                update.name.toLowerCase().includes(search.toLowerCase())
+            ))
+
         return (
-            <ol className='charity-list'>
-                {this.props.locations.map((location) => (
-                    <li key={location.id} className='charity-list-item'>
-                        <div
-                            className='org-image'
-                            style={{
-                                backgroundImage: `url(${location.imageURL})`
-                            }}
-                        ></div>
-                        <div className='organization-details'>
-                            <p>{location.name}</p>
-                            <p>{location.email}</p>
-                        </div>
-                        <button 
-                            onClick={() => this.props.onDeleteLocation(location)}
-                            className='organiation-remove'>
-                                Remove
-                        </button>
-                    </li>
-                ))}
-            </ol>
+            <div>
+                {/* {JSON.stringify(this.state)} */}
+                <div className=''>
+                    <input
+                        className="search-contacts"
+                        type="text"
+                        placeholder="Search Contacts"
+                        value={search}
+                        onChange={(event) => this.updateSearch(event.target.value)}
+                    />
+                </div>
+                <div className='charity-list'>
+                
+
+                    
+                    {showingAllOrganization.map((location) => (
+                        <li key={location.id} className='charity-list-item'>
+                            <div
+                                className='org-image'
+                                style={{
+                                    backgroundImage: `url(${location.imageURL})`
+                                }}
+                            ></div>
+                            <div className='organization-details'>
+                                <p>{location.name}</p>
+                                <p>{location.email}</p>
+                            </div>
+                            <button 
+                                onClick={() => onDeleteLocation(location)}
+                                className='organiation-remove'>
+                                    Remove
+                            </button>
+                        </li>
+                    ))}
+                </div>
+            </div>
+
         )
     }
-}
-
-DonationLists.propTypes = {
-    locations: PropTypes.array.isRequired,
-    onDeleteLocation: PropTypes.func.isRequired
 }
 
 export default DonationLists
